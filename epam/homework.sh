35bc3587a624451ab3e2fdee0f1e59dbf700a73e
Oleg Ivanov <xistadi@gmail.com>
2021-03-12 19:01:07 +0300

    step 8

--- epam/homework.sh
+++ epam/homework.sh
@@ -35,18 +35,18 @@ yum clean all && echo SUCCESS yun clean
 # set up RAID5 on 3 more drives(the size is not matter);
 yum install mdadm -y && echo SUCCESS install mdamd
 mdadm --create /dev/md5 --level=5 --raid-devices=3 /dev/sd[b-d]1 && echo SUCCESS create md device
-mkfs.ext4 /dev/md5 && echo SUCCESS create file system for raid devices
+mkfs.xfs /dev/md5 && echo SUCCESS create file system for raid devices
 mkdir /raid5 && echo SUCCESS create a mount point directory named raid5
 tempUUID=$(blkid /dev/md5 | cut -c 17-52) && echo SUCCESS generate UUID
-echo 'UUID='$tempUUID'     /raid5   ext4     defaults 0 0' >> /etc/fstab && echo SUCCESS mount the RAID using UUID
-mount -a && echo SUCCESS execute mount command
+echo 'UUID='$tempUUID'     /raid5   xfs     defaults 0 0' >> /etc/fstab && echo SUCCESS mount the RAID using UUID
+# mount -a && echo SUCCESS execute mount command
 mdadm --detail --scan --verbose >>  /etc/mdadm.conf && echo SUCCESS save the raid configuration
 
 
 # set up LVM for the RAID
-pvcreate /dev/sd[b-d]1 && echo SUCCESS physical volume  create
-vgcreate vg1 /dev/sd[b-d]1 && echo SUCCESS volume group create
-lvcreate -n lvr5 --type raid5 -l 100%FREE -i 2 vg1  && echo SUCCESS logical volume create
+pvcreate /dev/md5 && echo SUCCESS physical volume  create
+vgcreate vg1 /dev/md5 && echo SUCCESS volume group create
+lvcreate -n lvr5 -l 100%FREE vg1  && echo SUCCESS logical volume create
 
 
 # create xfs on top of it
@@ -55,7 +55,7 @@ mkfs.xfs /dev/vg1/lvr5 && echo SUCCESS format xfs
 
 # mount it to any folder;
 mkdir /mnt/lvr5 && echo SUCCESS mkdir for raid mount
-mount -t ext4 /dev/vg1/lvr5 /mnt/lvr5 && echo SUCCESS mount for raid
+mount -t xfs /dev/vg1/lvr5 /mnt/lvr5 && echo SUCCESS mount for raid
 
 
 # explode the folder into the network with NFS
