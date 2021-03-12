7d7833ccb623ef445c1d14070a3e18c39eae6b59
Oleg Ivanov <xistadi@gmail.com>
2021-03-12 16:46:17 +0300

    step 7

--- epam/homework.sh
+++ epam/homework.sh
@@ -19,7 +19,7 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT && echo SUCCESS single ssh port
 
 # add local DVD ISO as an repo(all internet repos need to be removed);
 yum -y install wget && echo SUCCESS install wget
-wget -P /home http://mirror.datacenter.by/pub/CentOS/7.9.2009/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso && echo SUCCESS download iso
+# wget -P /home http://mirror.datacenter.by/pub/CentOS/7.9.2009/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso && echo SUCCESS download iso
 mkdir /cdrom && echo SUCCESS mkdir
 mount /home/CentOS-7-x86_64-DVD-2009.iso /cdrom && echo SUCCESS mount
 rm -f /etc/yum.repos.d/*.repo && echo SUCCESS remove all repos
@@ -41,3 +41,32 @@ tempUUID=$(blkid /dev/md5 | cut -c 17-52) && echo SUCCESS generate UUID
 echo 'UUID='$tempUUID'     /raid5   ext4     defaults 0 0' >> /etc/fstab && echo SUCCESS mount the RAID using UUID
 mount -a && echo SUCCESS execute mount command
 mdadm --detail --scan --verbose >>  /etc/mdadm.conf && echo SUCCESS save the raid configuration
+
+
+# set up LVM for the RAID
+pvcreate /dev/sd[b-d]1 && echo SUCCESS physical volume  create
+vgcreate vg1 /dev/sd[b-d]1 && echo SUCCESS volume group create
+lvcreate -n lvr5 --type raid5 -l 100%FREE -i 2 vg1  && echo SUCCESS logical volume create
+
+
+# create xfs on top of it
+mkfs.xfs /dev/vg1/lvr5 && echo SUCCESS format xfs
+
+
+# mount it to any folder;
+mkdir /mnt/lvr5 && echo SUCCESS mkdir for raid mount
+mount -t ext4 /dev/vg1/lvr5 /mnt/lvr5 && echo SUCCESS mount for raid
+
+
+# explode the folder into the network with NFS
+yum install nfs-utils nfs-utils-lib -y  && echo SUCCESS install nfs
+sudo systemctl enable rpcbind
+sudo systemctl enable nfs-server
+sudo  systemctl enable nfs-lock
+sudo systemctl enable nfs-idmap
+sudo systemctl start rpcbind
+sudo systemctl start nfs-server
+sudo systemctl start nfs-lock
+sudo systemctl start nfs-idmap
+echo '/mnt/lvr5 192.168.10.0/24(rw,sync,no_root_squash,no_all_squash)' >> /etc/exports
+systemctl restart nfs-server
