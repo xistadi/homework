defa920866c894ef81db60c8a9ac7ff874d3c265
Oleg Ivanov <xistadi@gmail.com>
2021-03-18 10:10:53 +0300

    fix oop

--- Python trainee 1/homework.py
+++ Python trainee 1/homework.py
@@ -6,7 +6,10 @@ class Parser:
     """Класс для парсинга html страниц"""
 
     def __init__(self):
-        pass
+        self.count = 0
+        self.count_python = 0
+        self.count_linux = 0
+        self.count_flask = 0
 
     @staticmethod
     def get_main_text(url):
@@ -16,8 +19,7 @@ class Parser:
         main_text = source.text  # декодируем
         return main_text
 
-    @staticmethod
-    def parse_jobs_tut_by():
+    def parse_jobs_tut_by(self):
         """Парс jobs.tut.by вакансий по ключевому слову"""
         key = input('Какую вакансию ищем: ')  # получаем значения keyword с клавиатуры
         url = f'https://rabota.by/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text={key}'
@@ -27,39 +29,35 @@ class Parser:
         links = soup.find('div', {'class': 'vacancy-serp'}).findAll('a', {'class': 'HH-LinkModifier'})
         if len(links) == 0:  # если по ключевому слову ничего не найдено
             print('По введенному ключевому слову ничего не найдено!')
-        else:
-            # инициализация счетчиков
-            count = 0
-            count_python = 0
-            count_linux = 0
-            count_flask = 0
+        else:  # если же что-то нашли
             for link in links:  # проход по каждому полученному линку
                 second_url = link.get('href')  # получение значения href из строки
-                count += 1
+                self.count += 1
                 second_main_text = Parser.get_main_text(
                     second_url)  # получаем html страницы по получившемуся second_url
-                second_main_text_split = second_main_text.split()  # разбиваем слова из html по пробелу
+                second_main_text_split = second_main_text.lower().split()  # разбиваем слова из html по пробелу
                 for w in second_main_text_split:  # проходимся по каждому слову и нахождении слова прибавляем к счетчику
-                    if 'python' in w.lower():
-                        count_python += 1
-                    elif 'linux' in w.lower():
-                        count_linux += 1
-                    elif 'flask' in w.lower():
-                        count_flask += 1
+                    if 'python' in w:
+                        self.count_python += 1
+                    elif 'linux' in w:
+                        self.count_linux += 1
+                    elif 'flask' in w:
+                        self.count_flask += 1
                 print('==========================\n' + second_url)
             # вывод полученных значений
-            print('\n==========================\n' + f'Просмотренно вакансий: {count}')
-            print(f'Упоминаний \'python\': {count_python}')
-            print(f'Упоминаний \'linux\': {count_linux}')
-            print(f'Упоминаний \'flask\': {count_flask}' + '\n==========================')
-            print('AVG python: ' + str(round((count_python / count), 1)))
-            print('AVG linux: ' + str(round((count_linux / count), 1)))
-            print('AVG flask: ' + str(round((count_flask / count), 1)) + '\n==========================')
-
+            print('\n==========================\n' + f'Просмотренно вакансий: {self.count}')
+            print(f'Упоминаний \'python\': {self.count_python}')
+            print(f'Упоминаний \'linux\': {self.count_linux}')
+            print(f'Упоминаний \'flask\': {self.count_flask}' + '\n==========================')
+            print('AVG python: ' + str(round((self.count_python / self.count), 1)))
+            print('AVG linux: ' + str(round((self.count_linux / self.count), 1)))
+            print('AVG flask: ' + str(round((self.count_flask / self.count), 1)) + '\n==========================')
+            
 
 def main():
     """Основная (начальная) точка"""
-    Parser.parse_jobs_tut_by()
+    a = Parser()
+    a.parse_jobs_tut_by()
 
 
 if __name__ == "__main__":
